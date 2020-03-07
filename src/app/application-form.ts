1e374d2863717a488216ffa662db10076a3d2d36
Haritha Wickremasinghe <gawicks@gmail.com>
2020-03-07 12:58:06 +0530

    Remove validation functions from model

--- src/app/application-form.ts
+++ src/app/application-form.ts
@@ -8,24 +8,5 @@ export class ApplicationForm {
         public telephone: string = '',
         public expectedSalary?: number 
     ){}
-
-    public isAttributeValid(fieldName: string){
-        switch(fieldName){
-          case "name":
-            return !isNullOrEmtpyString(this.name);
-          case "address":
-            return !isNullOrEmtpyString(this.address);
-          case "email":
-            return !isNullOrEmtpyString(this.email) &&  this.email.match(/\S+@\S+\.\S+/);
-          case "telephone":
-            return !isNullOrEmtpyString(this.email) &&  this.telephone.match(/\+?[0-9]{10,12}/);
-          default:
-              return true;
-        }
-      }
-    
-    public isValid(){
-        return !Object.keys(this).some(key => !this.isAttributeValid(key));
-    }
         
 }
